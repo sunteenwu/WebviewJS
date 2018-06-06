c02ad6063c60e45e5990bf3e8435519ed34b0e1d
sunteenwu <wu_15xun@163.com>
2018-06-06 15:11:48 +0800

    New project

---
+++ WebViewCommunication/WebViewCommunication/MainApp.Connector/HtmlCommunicator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.ApplicationModel;
+using Windows.Foundation.Metadata;
+
+
+namespace MainApp.Connector
+{
+    public delegate void NotifyAppHandler(string str);
+    [AllowForWeb]
+    public sealed class HtmlCommunicator
+    {
+        public event NotifyAppHandler OnNotifyApp;
+        public void getHtmlSpecificEvent(string str)
+        {
+            OnNotifyApp?.Invoke(str);
+            // do something else
+        }
+
+        public string getAppVersion()
+        {
+            PackageVersion version = Package.Current.Id.Version;
+            return String.Format("{0}.{1}.{2}.{3}",
+                                 version.Major, version.Minor, version.Build, version.Revision);
+        }
+
+    } 
+
+}
---
+++ WebViewCommunication/WebViewCommunication/MainApp/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MainApp.Connector;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MainApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            string src = "ms-appx-web:///HTMLPage.html";
+            this.HtmlWebView.Navigate(new Uri(src));
+        }
+   
+
+        private HtmlCommunicator communicationWinRT = new HtmlCommunicator();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e); 
+            communicationWinRT.OnNotifyApp += CommunicationWinRT_OnNotifyApp; ;
+        }
+
+        private async void CommunicationWinRT_OnNotifyApp(string str)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                await HtmlWebView.InvokeScriptAsync("eval", new string[] { "doSomething()" });
+            });
+        }
+
+        private void HtmlWebView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            this.HtmlWebView.AddWebAllowedObject("SCObject", communicationWinRT); 
+  
+        }
+
+        private async void btntest_Click(object sender, RoutedEventArgs e)
+        {
+            await HtmlWebView.InvokeScriptAsync("eval", new string[] { "doSomething()" });
+        }
+    }
+}
